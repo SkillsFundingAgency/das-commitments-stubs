0e36285636a5ae303fb90eefbfea2087f7f29ab4
Alasdair Reid <ali.reid@live.co.uk>
2022-06-14 10:05:42 +0100

    Adding delivery model stub

--- modules/apim-endpoints/approvals/module.js
+++ modules/apim-endpoints/approvals/module.js
@@ -14,4 +14,30 @@ module.exports = function(app) {
 
     });
 
+
+    app.get('/apim-endpoints/approvals/Providers/:providerId/courses/:courseCode',(req, res) => {
+
+        let providerId = req.params.providerId;
+        let courseCode = req.params.courseCode;
+
+        console.log(string.format("providerId {0}", providerId));
+        console.log(string.format("courseCode {0}", courseCode));
+
+        files.sendFile(res, '/modules/apim-endpoints/approvals/delivery-models.json');
+
+    });
+
+
+    app.get('/apim-endpoints/approvals/rofjaa/agency/:legalEntityId',(req, res) => {
+
+        let legalEntityId = req.params.legalEntityId;
+
+        console.log(string.format("legalEntityId {0}", legalEntityId));
+
+        files.sendFile(res, '/modules/apim-endpoints/approvals/fjaa-agency.json');
+
+    });
+
+
+
 };
