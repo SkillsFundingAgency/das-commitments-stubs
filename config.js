66eba46c1e1eecb958dfdee0e431ba11bd1b1838
chrisfoster76 <chrisfoster186@googlemail.com>
2022-07-12 12:59:54 +0100

    Adds reservations UI stub

--- config.js
+++ config.js
@@ -2,5 +2,30 @@ let config = {};
 
 ///port -> what port the application should listen on
 config.port = 3999;
+config.sslport = 4000;
 
 module.exports = config;
+
+
+
+//reservations UI config
+
+config.providerCommitmentsBaseUrl = "https://localhost:5001";
+config.employerCommitmentsBaseUrl = "https://localhost:44376";
+
+config.levyaccounts = ['8194', '8193'];
+config.hashedLevyAccounts = ['VN48RP'];
+config.levyaccountlegalentities = ['2817', '2818'];
+config.hashedlevyaccountlegalentities = ['XEGE5X','XJGZ72'];
+
+
+config.employerReservations = [
+    {
+        accountId: "VNR6P9",
+        accountLegalEntityId: "X9JE72",
+        title: 'Test Reservation for Rapid Logistics',
+        subtitle: 'Supply Chain Leadership Professional',
+        courseCode: '398',
+        startMonthYear: '072020'
+    }
+];
