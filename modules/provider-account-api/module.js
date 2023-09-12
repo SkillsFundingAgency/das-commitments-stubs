22dce1bf5a9547d3290b022a05d9cb34de4f6158
chrisfoster76 <chrisfoster186@googlemail.com>
2023-09-12 08:29:51 +0100

    Adds provider account email send endpoint

--- modules/provider-account-api/module.js
+++ modules/provider-account-api/module.js
@@ -1,9 +1,19 @@
 /* Provider account api */
+const string = require('../shared/string');
 const files = require('../shared/files');
 
 module.exports = function(app) {
 
-    app.get('/provider-account-api/*',(req, res) => {
+    app.post('/provider-account-api/api/email/:providerId/send',(req, res) => {
+
+        let providerId = req.params.providerId;
+        console.log(string.format("Provider Account Api Send Email to Provider {0}", providerId));
+        res.status(200).send();
+        
+    });
+
+
+        app.get('/provider-account-api/*',(req, res) => {
         files.sendResponseFile(res, req.url, req.method);
     });
 
