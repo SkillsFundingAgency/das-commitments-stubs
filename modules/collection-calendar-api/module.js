1de3d1ae13fcad21f92dd36997f09c4edd4e57fb
Paul Graham <paul.graham@coprime.co.uk>
2024-10-01 16:13:46 +0100

    added collection calendar endpoint

---
+++ modules/collection-calendar-api/module.js
@@ -0,0 +1,12 @@
+/* Collection Calendar api */
+const files = require('../shared/files');
+
+module.exports = function(app) {
+
+    app.get("/collection-calendar-api/*", (req, res) => {
+
+        console.log("Get collection calendar dates ");
+
+        files.sendFile(res, '\\modules\\collection-calendar-api\\dates.json');
+    });
+};
